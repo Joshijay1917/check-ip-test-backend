9f538a88e13c7f99692d219283016c37f4faa054
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-17 08:40:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const SLOT_DURATION = 20; // seconds
 const SECRET = "6392d28e799c85c929f3336df3b95473"
 
 const port = 3000;
-const wss = new WebSocketServer({ port });
+//const wss = new WebSocketServer({ port });
 
 function generateToken(rootId) {
     const slot = Math.floor(Date.now() / 1000 / SLOT_DURATION);
@@ -18,16 +18,16 @@ app.listen(port, () => {
     console.log("Server is running on port ", port);
 })
 
-wss.on("connection", (ws) => {
-  console.log("Client connected");
+//wss.on("connection", (ws) => {
+  //console.log("Client connected");
 
-  ws.send(JSON.stringify({ token: generateToken() }));
+  //ws.send(JSON.stringify({ token: generateToken() }));
 
-  const interval = setInterval(() => {
-    if (ws.readyState === ws.OPEN) {
-      ws.send(JSON.stringify({ token: generateToken() }));
-    }
-  }, SLOT_DURATION * 1000);
+  //const interval = setInterval(() => {
+   // if (ws.readyState === ws.OPEN) {
+    //  ws.send(JSON.stringify({ token: generateToken() }));
+   // }
+ // }, SLOT_DURATION * 1000);
 
-  ws.on("close", () => clearInterval(interval));
-});
+ // ws.on("close", () => clearInterval(interval));
+//});
